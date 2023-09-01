8484b959c7b342242fbc39ddd9553d43fec8c560
Mortem <a20203607@pucp.edu.pe>
2023-08-31 20:36:28 -0500

    lacosa2

---
+++ lab3_20203607/src/main/java/com/example/lab3_20203607/Lab320203607Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203607;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320203607Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320203607Application.class, args);
+    }
+
+}
---
+++ lab3_20203607/src/test/java/com/example/lab3_20203607/Lab320203607ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203607;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320203607ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
